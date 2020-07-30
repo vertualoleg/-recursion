63384c8fa0ddd1c3655625b93e62f7e1c2de723a
Vertual <olegvertual@gmail.com>
2020-07-30 08:19:42 +0300

    add function draw_eyes,what draw eyes

--- lab3/zadanie_one.py
+++ lab3/zadanie_one.py
@@ -2,6 +2,17 @@ from graph import *
 def draw_face(x,y,R,color:str):
     brushColor(color)
     circle(x,y,R)
-draw_face(250,250,150,"yellow")
+def draw_eyes(x,y,R,color:str):
+    brushColor(color)
+    circle(x,y,R)
+    brushColor(color)
+    circle(x+125,y,R-15)
+    brushColor("black")
+    circle(x,y,R-R/2)
+    brushColor("black")
+    circle(x+125,y,R-30)
+    
 
+draw_face(250,250,150,"yellow")
+draw_eyes(190,215,40,"red")
 run()
